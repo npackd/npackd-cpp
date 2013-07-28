c0eaccb5aa3a851809a46290be94efb045375b2d
tim lebedkov <tim.lebedkov@googlemail.com>
2013-07-28 22:10:15 +0200

    64 bit build script

--- wpmcpp/Build64.bat
+++ wpmcpp/Build64.bat
@@ -1,68 +1,70 @@
-rem clone the source code repository
-rem set onecmd="%npackd_cl%\npackdcl.exe" "path" "--package=com.selenic.mercurial.Mercurial64" "--versions=[2, 3)"
-rem for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set mercurial=%%x
-rem "%mercurial%\hg.exe" clone https://code.google.com/p/windows-package-manager.npackd-cpp/ a
-rem if %errorlevel% neq 0 exit /b %errorlevel%
-
-set onecmd="%npackd_cl%\npackdcl.exe" "path" "--package=com.nokia.QtDev-i686-w64-Npackd-Release" "--versions=[4.8.2, 4.8.2)"
+set onecmd="%npackd_cl%\npackdcl.exe" "path" "--package=com.nokia.QtDev-x86_64-w64-Npackd-Release" "--versions=[4.8.2, 4.8.2]"
 for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set qt=%%x
-if %errorlevel% neq 0 exit /b %errorlevel%
 
-set onecmd="%npackd_cl%\npackdcl.exe" "path" "--package=com.nokia.QtDev-x86_64-w64-Npackd-Release" "--versions=[4.8.2, 4.8.2)"
-for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set qt64=%%x
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-set onecmd="%npackd_cl%\npackdcl.exe" "path" "--package=net.sourceforge.mingw-w64.MinGWW64" "--versions=[4.7.2, 4.7.2)"
+set onecmd="%npackd_cl%\npackdcl.exe" "path" "--package=net.sourceforge.mingw-w64.MinGWW6464" "--versions=[4.7.2, 4.7.2]"
 for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set mingw=%%x
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-set onecmd="%npackd_cl%\npackdcl.exe" "path" "--package=net.sourceforge.mingw-w64.MinGWW6464" "--versions=[4.7.2, 4.7.2)"
-for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set mingw64=%%x
-if %errorlevel% neq 0 exit /b %errorlevel%
 
 set onecmd="%npackd_cl%\npackdcl.exe" "path" "--package=com.advancedinstaller.AdvancedInstallerFreeware" "--versions=[10, 11)"
 for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set ai=%%x
-if %errorlevel% neq 0 exit /b %errorlevel%
 
-set onecmd="%npackd_cl%\npackdcl.exe" "path" "--package=org.7-zip.SevenZIP64" "--versions=[9, 10)"
+set onecmd="%npackd_cl%\npackdcl.exe" "path" "--package=org.7-zip.SevenZIP" "--versions=[9, 10)"
 for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set sevenzip=%%x
-if %errorlevel% neq 0 exit /b %errorlevel%
 
-rmdir /S /Q build
+set onecmd="%npackd_cl%\npackdcl.exe" "path" "--package=quazip-dev-x86_64-w64-static" "--versions=[0.5, 0.5]"
+for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set quazip=%%x
+
+set quazip_path=%quazip%
 
 mkdir build
-if %errorlevel% neq 0 exit /b %errorlevel%
+mkdir build\64
 
-mkdir build\release32
-if %errorlevel% neq 0 exit /b %errorlevel%
+set path=%mingw%\bin
+cd build\64
+"%qt%\qmake\qmake.exe" ..\..\wpmcpp.pro -r -spec win32-g++ CONFIG+=release
+if %errorlevel% neq 0 goto error
 
-set path=%qt_path%\mingw\bin
-cd build\release32
-"%qt_path%\Desktop\Qt\4.7.3\mingw\bin\make.exe" wpmcpp.pro -r -spec win32-g++ CONFIG+=release
-if %errorlevel% neq 0 exit /b %errorlevel%
+"%mingw%\bin\mingw32-make.exe"
+if %errorlevel% neq 0 goto error
 
-"%qt_path%\mingw\bin\mingw32-make.exe"
-if %errorlevel% neq 0 exit /b %errorlevel%
+"%qt%\bin\lrelease.exe" ..\..\wpmcpp.pro
+if %errorlevel% neq 0 goto error
+
+mkdir zip
+
+copy ..\..\wpmcpp_es.qm zip\npackdg_es.qm
+if %errorlevel% neq 0 goto error
+
+copy ..\..\wpmcpp_ru.qm zip\npackdg_ru.qm
+if %errorlevel% neq 0 goto error
+
+copy ..\..\wpmcpp_fr.qm zip\npackdg_fr.qm
+if %errorlevel% neq 0 goto error
+
+copy ..\..\wpmcpp_de.qm zip\npackdg_de.qm
+if %errorlevel% neq 0 goto error
 
 cd ..\..
 
-copy LICENSE.txt .Build\32\
-if %errorlevel% neq 0 goto out
+copy LICENSE.txt build\64\zip
 
-copy CrystalIcons_LICENSE.txt .Build\32\
-if %errorlevel% neq 0 goto out
+copy CrystalIcons_LICENSE.txt build\64\zip
+if %errorlevel% neq 0 goto error
 
-copy "..\wpmcpp-build-desktop\release\wpmcpp.exe" .Build\32\npackdg.exe
-if %errorlevel% neq 0 goto out
+copy build\64\release\wpmcpp.exe build\64\zip\npackdg.exe
+if %errorlevel% neq 0 goto error
+
+"%mingw%\bin\strip.exe" build\64\zip\npackdg.exe
+if %errorlevel% neq 0 goto error
 
 rem creating .zip
-cd build\32\
-"%sevenzipa_path%\7za" a ..\Npackd.zip *
-if %errorlevel% neq 0 goto out
-cd ..\..
+cd build\64\zip
+"%sevenzip%\7z" a ..\Npackd64.zip *
+if %errorlevel% neq 0 goto error
+cd ..\..\..
 
 rem creating .msi
-"%ai_path%\bin\x86\AdvancedInstaller.com" /build wpmcpp.aip
-if %errorlevel% neq 0 goto out
+"%ai%\bin\x86\AdvancedInstaller.com" /build wpmcpp64.aip
+if %errorlevel% neq 0 goto error
 
+:error
 
