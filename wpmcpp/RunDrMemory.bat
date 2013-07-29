8192a6548ca6a463da7e840f9abb71823097ae79
tim lebedkov <tim.lebedkov@googlemail.com>
2013-07-29 20:26:30 +0200

    remove unused scripts

--- wpmcpp/RunDrMemory.bat
+++ wpmcpp/RunDrMemory.bat
@@ -1,5 +1,4 @@
-cd nd-build-desktop\debug
-"C:\Program Files (x86)\Dr. Memory\bin\drmemory.exe" -no_callstack_use_top_fp -- nd.exe 
-rem find-updates
+cd %~dp0
+"C:\Program Files (x86)\Dr. Memory\bin\drmemory.exe" -no_callstack_use_top_fp -- "%CD%\..\wpmcpp-i686\release\wpmcpp.exe"
 pause
 
