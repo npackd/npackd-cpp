83c7109847f96c51bcec68ebf66cca836cf3a8d7
tim lebedkov <tim.lebedkov@googlemail.com>
2014-08-14 11:02:20 +0200

    more tests

--- npackdcl/ftests/src/app.cpp
+++ npackdcl/ftests/src/app.cpp
@@ -44,6 +44,15 @@ int App::functionalTest()
         WPMUtils::outputTextConsole("---- test succeeded\n");
     }
 
+    WPMUtils::outputTextConsole("---- testing add twice\n");
+    if (!captureNpackdCLOutput("add -p io.mpv.mpv-64 -v 0.4").
+            contains("is already installed in")) {
+        WPMUtils::outputTextConsole("---- test failed\n", false);
+        r++;
+    } else {
+        WPMUtils::outputTextConsole("---- test succeeded\n");
+    }
+
     WPMUtils::outputTextConsole("---- testing rm\n");
     if (!captureNpackdCLOutput("rm -p io.mpv.mpv-64 -v 0.4").
             contains("removed successfully")) {
@@ -95,6 +104,68 @@ int App::functionalTest()
         WPMUtils::outputTextConsole("---- test succeeded\n");
     }
 
+    WPMUtils::outputTextConsole("---- testing add-repo\n");
+    if (!captureNpackdCLOutput("add-repo --url http://localhost:8083/NonExisting.xml").
+            contains("The repository was added successfully")) {
+        WPMUtils::outputTextConsole("---- test failed\n", false);
+        r++;
+    } else {
+        WPMUtils::outputTextConsole("---- test succeeded\n");
+    }
+
+    WPMUtils::outputTextConsole("---- testing remove-repo\n");
+    if (!captureNpackdCLOutput("remove-repo --url http://localhost:8083/NonExisting.xml").
+            contains("The repository was removed successfully")) {
+        WPMUtils::outputTextConsole("---- test failed\n", false);
+        r++;
+    } else {
+        WPMUtils::outputTextConsole("---- test succeeded\n");
+    }
+
+    WPMUtils::outputTextConsole("---- testing search\n");
+    if (!captureNpackdCLOutput("search -q \"c++\"").
+            contains("Microsoft Visual C++")) {
+        WPMUtils::outputTextConsole("---- test failed\n", false);
+        r++;
+    } else {
+        WPMUtils::outputTextConsole("---- test succeeded\n");
+    }
+
+    WPMUtils::outputTextConsole("---- testing search -b\n");
+    if (captureNpackdCLOutput("search -q \"c++\" -b").
+            contains("packages found")) {
+        WPMUtils::outputTextConsole("---- test failed\n", false);
+        r++;
+    } else {
+        WPMUtils::outputTextConsole("---- test succeeded\n");
+    }
+
+    WPMUtils::outputTextConsole("---- testing info\n");
+    if (!captureNpackdCLOutput("info -p io.mpv.mpv").
+            contains("command line movie player")) {
+        WPMUtils::outputTextConsole("---- test failed\n", false);
+        r++;
+    } else {
+        WPMUtils::outputTextConsole("---- test succeeded\n");
+    }
+
+    WPMUtils::outputTextConsole("---- testing list-repos\n");
+    if (!captureNpackdCLOutput("list-repos").
+            contains("npackd.appspot.com")) {
+        WPMUtils::outputTextConsole("---- test failed\n", false);
+        r++;
+    } else {
+        WPMUtils::outputTextConsole("---- test succeeded\n");
+    }
+
+    WPMUtils::outputTextConsole("---- testing check\n");
+    if (!captureNpackdCLOutput("check").
+            contains("All dependencies are installed")) {
+        WPMUtils::outputTextConsole("---- test failed\n", false);
+        r++;
+    } else {
+        WPMUtils::outputTextConsole("---- test succeeded\n");
+    }
 
     WPMUtils::outputTextConsole(QString("---- %1 tests failed\n").arg(r));
 
