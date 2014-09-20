18023f80dc016e89a971c72ac49a57c4e10b68a5
tim lebedkov <tim.lebedkov@googlemail.com>
2014-09-20 13:29:24 +0200

    support for .tar.gz packages

--- vimorgrep/src/vimorgrepapp.h
+++ vimorgrep/src/vimorgrepapp.h
@@ -2,10 +2,20 @@
 #define VIMORGREPAPP_H
 
 #include <QObject>
+#include <QString>
+
+#include "packageversion.h"
 
 class VimOrgRepApp: public QObject
 {
     Q_OBJECT
+    PackageVersion *createZIPPackage(const QString &packageName, const QString &scriptVersion, const QString &jpackage, const QString &srcId,
+                                     const QString &vimVersion);
+    PackageVersion *createTarGZPackage(const QString &packageName, const QString &scriptVersion, const QString &jpackage, const QString &srcId, const QString &vimVersion);
+    PackageVersion *createVimSyntaxPackage(const QString &packageName,
+            const QString &scriptVersion, const QString &jpackage,
+            const QString &srcId, const QString &vimVersion,
+            const QString& scriptType);
 public:
     VimOrgRepApp();
 public slots:
