7b7780d7fe6d1aa737c9e8410776fbed4735b1bf
tim lebedkov <tim.lebedkov@googlemail.com>
2014-09-28 21:20:28 +0200

    progress in a tree

---
+++ wpmcpp/src/progresstree2.h
@@ -0,0 +1,47 @@
+#ifndef PROGRESSTREE2_H
+#define PROGRESSTREE2_H
+
+#include <time.h>
+
+#include <QTreeWidget>
+#include <QTimer>
+#include <QThread>
+#include <QString>
+#include <QWidget>
+#include <QString>
+
+#include "job.h"
+
+/**
+ * @brief tree with the running jobs
+ */
+class ProgressTree2: public QTreeWidget
+{
+    Q_OBJECT
+private:
+    time_t started;
+    QTimer* timer;
+    QString title;
+    QThread* thread;
+    time_t monitoredJobLastChanged;
+    void updateItem(QTreeWidgetItem *item, const JobState &s);
+public:
+    /**
+     * @param parent parent widget
+     */
+    ProgressTree2(QWidget* parent);
+
+    /**
+     * @brief registers a new job
+     * @param job the job
+     * @param title job title
+     * @param thread job thread
+     */
+    void addJob(Job* job, const QString& title, QThread* thread);
+private slots:
+    void cancelClicked();
+    void monitoredJobChanged(const JobState &state);
+    void threadFinished();
+};
+
+#endif // PROGRESSTREE2_H
