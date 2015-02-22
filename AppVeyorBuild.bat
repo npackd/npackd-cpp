4353852e5bd3ac08ea505466828b643f0332ee44
tim lebedkov <tim.lebedkov@googlemail.com>
2015-02-22 11:00:56 +0100

    fix build 2

--- AppVeyorBuild.bat
+++ AppVeyorBuild.bat
@@ -5,12 +5,12 @@ rem %1 - type of the target system: 32 or 64
 
 if %bits% equ 64 goto bits64
 rem Npackd
-set make=C:\Program Files (x86)\MinGW-w64_i686_SJLJ_POSIX_threads\bin\mingw-make.exe
+set make=C:\Program Files (x86)\MinGW-w64_i686_SJLJ_POSIX_threads\bin\mingw32-make.exe
 "%make%" -C wpmcpp zip msi PROFILE=release32
 "%make%" -C npackdcl\compile PROFILE=release32
 goto :eof
 
 bits64:
-set mingw=C:\Program Files (x86)\MinGW-w64_x86_64_SEH_POSIX_threads
-"%mingw%\bin\mingw32-make.exe" -C wpmcpp zip msi PROFILE=release64
+set make=C:\Program Files (x86)\MinGW-w64_x86_64_SEH_POSIX_threads\bin\mingw32-make.exe"
+"%make%" -C wpmcpp zip msi PROFILE=release64
 
--- AppVeyorTest.bat
+++ AppVeyorTest.bat
@@ -4,7 +4,7 @@ rem Parameters:
 rem %1 - type of the target system: 32 or 64
 
 if %bits% equ 64 goto bits64
-set make=C:\Program Files (x86)\MinGW-w64_i686_SJLJ_POSIX_threads
+set make=C:\Program Files (x86)\MinGW-w64_i686_SJLJ_POSIX_threads\bin\mingw32-make.exe
 "%make%" -C npackdcl\tests compile PROFILE=release32
 npackdcl\tests\build\32\release\tests -v2
 "%make%" -C npackdcl\ftests compile PROFILE=release32
