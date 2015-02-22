11653168110126e04acb2a51ab56513878ea8903
tim lebedkov <tim.lebedkov@googlemail.com>
2015-02-22 11:39:16 +0100

    fix build 3

--- AppVeyorInstall.bat
+++ AppVeyorInstall.bat
@@ -5,7 +5,7 @@ rem Parameters:
 rem %1 - type of the target system: 32 or 64
 
 msiexec.exe /qn /i http://bit.ly/npackdcl-1_19_13 || exit /b %errorlevel%
-SET NPACKD_CL=C:\Program Files (x86)\NpackdCL || exit /b %errorlevel%
+SET NPACKD_CL=C:\Program Files (x86)\NpackdCL|| exit /b %errorlevel%
 "%npackd_cl%\ncl" add-repo --url=https://npackd.appspot.com/rep/recent-xml || exit /b %errorlevel%
 "%npackd_cl%\ncl" add-repo --url=https://npackd.appspot.com/rep/xml?tag=libs || exit /b %errorlevel%
 "%npackd_cl%\ncl" detect || exit /b %errorlevel%
