38dac6942f5c090c0351f38a0596125acf0f0130
tim lebedkov <tim.lebedkov@googlemail.com>
2015-02-24 20:22:29 +0100

    changed the order for PushArtifact

--- AppVeyorBuild.bat
+++ AppVeyorBuild.bat
@@ -6,9 +6,11 @@ if %bits% equ 64 goto bits64
 rem Npackd
 set make=C:\Program Files (x86)\MinGW-w64_i686_SJLJ_POSIX_threads\bin\mingw32-make.exe
 
+where appveyor
+
 "%make%" -C wpmcpp zip msi PROFILE=release32 || exit /b %errorlevel%
-appveyor PushArtifact wpmcpp\build\32\release\Npackd32-%version%.msi || exit /b %errorlevel%
 appveyor PushArtifact wpmcpp\build\32\release\Npackd32-%version%.zip || exit /b %errorlevel%
+appveyor PushArtifact wpmcpp\build\32\release\Npackd32-%version%.msi || exit /b %errorlevel%
 appveyor PushArtifact wpmcpp\build\32\release\Npackd32-%version%.map || exit /b %errorlevel%
 
 "%make%" -C npackdcl zip msi PROFILE=release32 || exit /b %errorlevel%
