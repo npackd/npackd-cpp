0a413ece32ef048829dea87fd1eca91e0d37094a
tim lebedkov <tim.lebedkov@googlemail.com>
2015-02-24 21:49:20 +0100

    split NpackdCL build

--- AppVeyorBuild.bat
+++ AppVeyorBuild.bat
@@ -2,28 +2,31 @@ echo on
 
 rem This script is used by AppVeyor to build the project.
 
-if %bits% equ 64 goto bits64
-rem Npackd
-set make=C:\Program Files (x86)\MinGW-w64_i686_SJLJ_POSIX_threads\bin\mingw32-make.exe
-
 where appveyor
 
-"%make%" -C wpmcpp zip msi PROFILE=release32 || exit /b %errorlevel%
-appveyor PushArtifact wpmcpp\build\32\release\Npackd32-%version%.zip || exit /b %errorlevel%
-appveyor PushArtifact wpmcpp\build\32\release\Npackd32-%version%.msi || exit /b %errorlevel%
-appveyor PushArtifact wpmcpp\build\32\release\Npackd32-%version%.map || exit /b %errorlevel%
+if %bits% equ 64 goto bits64
 
-"%make%" -C npackdcl zip msi PROFILE=release32 || exit /b %errorlevel%
-appveyor PushArtifact npackdcl\build\32\release\NpackdCL32-%version%.zip || exit /b %errorlevel%
-appveyor PushArtifact npackdcl\build\32\release\NpackdCL32-%version%.msi || exit /b %errorlevel%
-appveyor PushArtifact npackdcl\build\32\release\NpackdCL32-%version%.map || exit /b %errorlevel%
-
-goto :eof
+set make=C:\Program Files (x86)\MinGW-w64_i686_SJLJ_POSIX_threads\bin\mingw32-make.exe
+goto start
 
 :bits64
 set make=C:\Program Files (x86)\MinGW-w64_x86_64_SEH_POSIX_threads\bin\mingw32-make.exe
-"%make%" -C wpmcpp zip msi PROFILE=release64 || exit /b %errorlevel%
-appveyor PushArtifact wpmcpp\build\64\release\Npackd64-%version%.msi || exit /b %errorlevel%
-appveyor PushArtifact wpmcpp\build\64\release\Npackd64-%version%.zip || exit /b %errorlevel%
-appveyor PushArtifact wpmcpp\build\64\release\Npackd64-%version%.map || exit /b %errorlevel%
+goto start
+
+:start
+if %prg% equ npackdcl goto npackdcl
+
+:npackd
+"%make%" -C wpmcpp zip msi PROFILE=release%bits% || exit /b %errorlevel%
+appveyor PushArtifact wpmcpp\build\%bits%\release\Npackd%bits%-%version%.zip || exit /b %errorlevel%
+appveyor PushArtifact wpmcpp\build\%bits%\release\Npackd%bits%-%version%.msi || exit /b %errorlevel%
+appveyor PushArtifact wpmcpp\build\%bits%\release\Npackd%bits%-%version%.map || exit /b %errorlevel%
+goto :eof
+
+:npackdcl
+"%make%" -C npackdcl zip msi PROFILE=release%bits% || exit /b %errorlevel%
+appveyor PushArtifact npackdcl\build\%bits%\release\NpackdCL%bits%-%version%.zip || exit /b %errorlevel%
+appveyor PushArtifact npackdcl\build\%bits%\release\NpackdCL%bits%-%version%.msi || exit /b %errorlevel%
+appveyor PushArtifact npackdcl\build\%bits%\release\NpackdCL%bits%-%version%.map || exit /b %errorlevel%
+goto :eof
 
--- AppVeyorTest.bat
+++ AppVeyorTest.bat
@@ -3,12 +3,23 @@ echo on
 rem This script is used by AppVeyor to build the project.
 
 if %bits% equ 64 goto bits64
+
 set make=C:\Program Files (x86)\MinGW-w64_i686_SJLJ_POSIX_threads\bin\mingw32-make.exe
+goto start
+
+:bits64
+set make=C:\Program Files (x86)\MinGW-w64_x86_64_SEH_POSIX_threads\bin\mingw32-make.exe
+goto start
+
+:start
+if %prg% equ npackdcl goto npackdcl
+goto :eof
+
+:npackdcl
 "%make%" -C npackdcl\tests compile PROFILE=release32 || exit /b %errorlevel%
 npackdcl\tests\build\32\release\tests -v2 || exit /b %errorlevel%
 "%make%" -C npackdcl\ftests compile PROFILE=release32 || exit /b %errorlevel%
 npackdcl\ftests\build\32\release\ftests -v2 || exit /b %errorlevel%
 goto :eof
 
-:bits64
 
