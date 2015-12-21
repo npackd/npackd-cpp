c3f733abf2b708e0c448d03ce9efcc4182c8d037
Tim Lebedkov <tim.lebedkov@gmail.com>
2015-12-21 22:48:52 +0100

    64 bit

--- AppVeyorTest.bat
+++ AppVeyorTest.bat
@@ -2,9 +2,9 @@ echo on
 
 rem This script is used by AppVeyor to build the project.
 
-if %bits% equ 64 goto bits64
-
 SET NPACKD_CL=C:\Program Files (x86)\NpackdCL
+
+if %bits% equ 64 goto bits64
 
 set make=C:\Program Files (x86)\MinGW-w64_i686_SJLJ_POSIX_threads\bin\mingw32-make.exe
 goto start
