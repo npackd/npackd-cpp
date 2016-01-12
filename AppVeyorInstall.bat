8b6d67baa155f58e3fc2900b746944034fe2b8bb
Tim Lebedkov <tim.lebedkov@gmail.com>
2016-01-12 17:53:00 +0100

    repair the coverity build

--- AppVeyorInstall.bat
+++ AppVeyorInstall.bat
@@ -21,14 +21,14 @@ goto :eof
 :bits64
 "%npackd_cl%\ncl" add -p npackd-dev-x86_64-w64 -v %version% || exit /b %errorlevel%
 
-if "%target%" equ "coverity" (
-    "%npackd_cl%\ncl" add -p com.github.bmatzelle.Gow -v 0.8
-    dir "C:\Program Files (x86)"
-    "C:\Program Files (x86)\Gow\bin\wget" https://scan.coverity.com/download/cxx/win_64 --post-data "token=%covtoken%&project=Npackd" -O coverity_tool.zip --no-check-certificate -nv
-    7z x -y coverity_tool.zip
-    for /f "delims=" %%x in ('dir /b cov-*') do set name=%%x
-    ren "%%name%%" cov-analysis
-)
+if "%target%" neq "coverity" goto end
+"%npackd_cl%\ncl" add -p com.github.bmatzelle.Gow -v 0.8
+dir "C:\Program Files (x86)"
+"C:\Program Files (x86)\Gow\bin\wget" https://scan.coverity.com/download/cxx/win_64 --post-data "token=%covtoken%&project=Npackd" -O coverity_tool.zip --no-check-certificate -nv
+7z x -y coverity_tool.zip
+for /f "delims=" %%x in ('dir /b cov-*') do set name=%%x
+ren "%%name%%" cov-analysis
 
+:end
 rem tree "C:\Program Files (x86)"
 
