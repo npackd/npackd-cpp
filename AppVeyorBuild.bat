8fe160b60d6fcfe277687550785d78dc3e393728
tim-lebedkov <tim.lebedkov@gmail.com>
2017-12-19 21:54:38 +0100

    build with CMake/appveyor 9

--- AppVeyorBuild.bat
+++ AppVeyorBuild.bat
@@ -2,6 +2,8 @@ echo on
 
 rem This script is used by AppVeyor to build the project.
 
+set initial_path=%path%
+
 where appveyor
 where cmake
 
@@ -11,6 +13,7 @@ if %bits% equ 64 goto bits64
 
 set QT=C:\NpackdSymlinks\com.nokia.QtDev-i686-w64-Npackd-Release-5.5
 set PACKAGE=com.googlecode.windows-package-manager.Npackd
+set mingw_libs=i686-w64-mingw32
 
 set onecmd="%npackd_cl%\npackdcl.exe" "path" "--package=mingw-w64-i686-sjlj-posix" "--versions=[4.9.2,4.9.2]"
 for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set mingw=%%x
@@ -27,6 +30,7 @@ goto start
 
 set QT=C:\NpackdSymlinks\com.nokia.QtDev-x86_64-w64-Npackd-Release-5.5
 set PACKAGE=com.googlecode.windows-package-manager.Npackd64
+set mingw_libs=x86_64-w64-mingw32
 
 set onecmd="%npackd_cl%\npackdcl.exe" "path" "--package=mingw-w64-x86_64-seh-posix" "--versions=[4.9.2,4.9.2]"
 for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set mingw=%%x
@@ -50,16 +54,16 @@ if "%target%" equ "coverity" goto coverity
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 tree . /f
-appveyor PushArtifact wpmcpp\build\%bits%\release\Npackd%bits%-%version%.zip
+appveyor PushArtifact wpmcpp\build\Npackd%bits%-%version%.zip
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-appveyor PushArtifact wpmcpp\build\%bits%\release\Npackd%bits%-%version%.msi
+appveyor PushArtifact wpmcpp\build\Npackd%bits%-%version%.msi
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-appveyor PushArtifact wpmcpp\build\%bits%\release\Npackd%bits%-%version%.map
+appveyor PushArtifact wpmcpp\build\Npackd%bits%-%version%.map
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-appveyor PushArtifact wpmcpp\build\%bits%\release\Npackd%bits%-debug-%version%.zip
+appveyor PushArtifact wpmcpp\build\Npackd%bits%-debug-%version%.zip
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 goto :eof
@@ -75,16 +79,15 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 goto :eof
 
 :npackdcl
-pushd
 mkdir npackdcl\build
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-cd npackdcl\build
+pushd npackdcl\build
 set path=%mingw%\bin;C:\Program Files (x86)\CMake\bin
 set qtdir=%qt:\=/%
 set quazip_home=%quazip:\=/%
 
-cmake ..\ -G "MinGW Makefiles" -DZLIB_LIBRARY:FILEPATH="%mingw%\x86_64-w64-mingw32\lib\libz.a" -DZLIB_INCLUDE_DIR:PATH="%mingw%\x86_64-w64-mingw32\include" -DCMAKE_INSTALL_PREFIX=%cd%\npackdcl\install"
+cmake ..\ -G "MinGW Makefiles" -DZLIB_LIBRARY:FILEPATH="%mingw%\%mingw_libs%\lib\libz.a" -DZLIB_INCLUDE_DIR:PATH="%mingw%\x86_64-w64-mingw32\include" -DCMAKE_INSTALL_PREFIX=%cd%\npackdcl\install"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 rem todo -C npackdcl zip msi zip-debug PROFILE=release%bits%
@@ -93,16 +96,18 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 popd
 
-appveyor PushArtifact npackdcl\build\release\NpackdCL-%version%.zip
+set path=%initial_path%
+
+appveyor PushArtifact npackdcl\build\NpackdCL-%version%.zip
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-appveyor PushArtifact npackdcl\build\release\NpackdCL-%version%.msi
+appveyor PushArtifact npackdcl\build\NpackdCL-%version%.msi
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-appveyor PushArtifact npackdcl\build\release\NpackdCL-%version%.map
+appveyor PushArtifact npackdcl\build\NpackdCL-%version%.map
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-appveyor PushArtifact npackdcl\build\release\NpackdCL%bits%-debug-%version%.zip
+appveyor PushArtifact npackdcl\build\NpackdCL%bits%-debug-%version%.zip
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 goto :eof
@@ -111,13 +116,13 @@ goto :eof
 "%make%" -C clu zip zip-debug PROFILE=release%bits%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-appveyor PushArtifact clu\build\%bits%\release\CLU-%version%.zip
+appveyor PushArtifact clu\build\CLU-%version%.zip
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-appveyor PushArtifact clu\build\%bits%\release\CLU-%version%.map
+appveyor PushArtifact clu\build\CLU-%version%.map
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-appveyor PushArtifact clu\build\%bits%\release\CLU%bits%-debug-%version%.zip
+appveyor PushArtifact clu\build\CLU%bits%-debug-%version%.zip
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 goto :eof
