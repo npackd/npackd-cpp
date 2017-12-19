79de0114f91c1fc8872c8f075662e4e7c1b4c5ce
tim-lebedkov <tim.lebedkov@gmail.com>
2017-12-19 20:22:40 +0100

    build with CMake/appveyor 3

--- AppVeyorBuild.bat
+++ AppVeyorBuild.bat
@@ -78,7 +78,7 @@ goto :eof
 mkdir npackdcl\build
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-cd npackdcl\build&&set path=%mingw%\bin&&cmake ..\ -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%cd%\npackdcl\install"
+cd npackdcl\build&&set path=%mingw%\bin;C:\Program Files (x86)\CMake\bin&&cmake ..\ -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%cd%\npackdcl\install"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 rem todo -C npackdcl zip msi zip-debug PROFILE=release%bits%
