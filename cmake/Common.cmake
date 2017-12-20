ae210f5b5437f253336f910ad50f2d5c450e0b4a
tim-lebedkov <tim.lebedkov@gmail.com>
2017-12-21 00:30:09 +0100

    no warnings

---
+++ cmake/Common.cmake
@@ -0,0 +1,11 @@
+# compiler warnings
+if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANGXX OR CMAKE_COMPILER_IS_GNUCC)
+  set(NPACKD_WARNING_FLAGS "-Wall -Winit-self -Wwrite-strings -Wextra -Wno-long-long -Wno-overloaded-virtual -Wno-missing-field-initializers -Wno-unused-parameter")
+  if(EMSCRIPTEN)
+    set(NPACKD_WARNING_FLAGS "${NPACKD_WARNING_FLAGS} -Wno-warn-absolute-paths")
+  elseif(NOT APPLE)
+    set(NPACKD_WARNING_FLAGS "${NPACKD_WARNING_FLAGS} -Wno-unused-but-set-parameter")
+  endif()
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${NPACKD_WARNING_FLAGS}")
+endif()
+
