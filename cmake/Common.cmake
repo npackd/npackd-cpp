b98995d2a1625d591b17b857b46e39e04d31d031
tim-lebedkov <tim.lebedkov@gmail.com>
2017-12-21 16:42:30 +0100

    less warnings on Appveyor

--- cmake/Common.cmake
+++ cmake/Common.cmake
@@ -1,6 +1,6 @@
 # compiler warnings
 if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANGXX OR CMAKE_COMPILER_IS_GNUCC)
-  set(NPACKD_WARNING_FLAGS "-Wall -Winit-self -Wwrite-strings -Wextra -Wno-long-long -Wno-overloaded-virtual -Wno-missing-field-initializers -Wno-unused-parameter")
+  set(NPACKD_WARNING_FLAGS "-Wall -Winit-self -Wwrite-strings -Wextra -Wno-long-long -Wno-overloaded-virtual -Wno-missing-field-initializers -Wno-unused-parameter -Wno-unknown-pragmas")
   if(EMSCRIPTEN)
     set(NPACKD_WARNING_FLAGS "${NPACKD_WARNING_FLAGS} -Wno-warn-absolute-paths")
   elseif(NOT APPLE)
