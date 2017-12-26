d239c619ba4251e002a9a3067ee4edafc58770c3
tim-lebedkov <tim.lebedkov@gmail.com>
2017-12-26 13:44:29 +0100

    CMake: reuse the code for reading the version number from version.txt

--- cmake/Common.cmake
+++ cmake/Common.cmake
@@ -9,3 +9,35 @@ if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANGXX OR CMAKE_COMPILER_IS_GN
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${NPACKD_WARNING_FLAGS}")
 endif()
 
+# Reads the version number from Version.txt
+#
+# txt: path to the Version.txt
+macro(readVersion txt)
+	file(STRINGS ${txt} READ_BUILD_NUMBER)
+	string(REGEX MATCHALL "[0-9]+" _versionComponents "${READ_BUILD_NUMBER}")
+	list(LENGTH _versionComponents _len)
+	if (${_len} GREATER 0)
+		list(GET _versionComponents 0 NPACKD_VERSION_MAJOR)
+	else()
+		set(NPACKD_VERSION_MAJOR 0)
+	endif()
+	if (${_len} GREATER 1)
+		list(GET _versionComponents 1 NPACKD_VERSION_MINOR)
+	else()
+		set(NPACKD_VERSION_MINOR 0)
+	endif()
+	if (${_len} GREATER 2)
+		list(GET _versionComponents 2 NPACKD_VERSION_PATCH)
+	else()
+		set(NPACKD_VERSION_PATCH 0)
+	endif()
+	if (${_len} GREATER 3)
+		list(GET _versionComponents 3 NPACKD_VERSION_TWEAK)
+	else()
+		set(NPACKD_VERSION_TWEAK 0)
+	endif()
+
+	set(NPACKD_VERSION "${NPACKD_VERSION_MAJOR}.${NPACKD_VERSION_MINOR}.${NPACKD_VERSION_PATCH}.${NPACKD_VERSION_TWEAK}")
+
+	message(STATUS "Build npackd v${NPACKD_VERSION}")
+endmacro(readVersion)
