12b4750430835f9d5f900db3198e24d4e5664d04
tim-lebedkov <tim.lebedkov@gmail.com>
2018-01-01 15:13:46 +0100

    Appveyor: search for dependencies in test

--- AppVeyorTest.bat
+++ AppVeyorTest.bat
@@ -7,6 +7,56 @@ if %prg% neq npackdcl goto :eof
 
 SET NPACKD_CL=C:\Program Files (x86)\NpackdCL
 
+set onecmd="%npackd_cl%\ncl.exe" path -p com.advancedinstaller.AdvancedInstallerFreeware -r [10,20)
+for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set ai=%%x
+
+set onecmd="%npackd_cl%\ncl.exe" path -p org.7-zip.SevenZIP -r [9,20)
+for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set SEVENZIP=%%x
+
+set onecmd="%npackd_cl%\ncl.exe" path -p exeproxy -r [0.2,1)
+for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set EXEPROXY=%%x
+
+if %bits% equ 64 goto bits64
+
+set QT=C:\NpackdSymlinks\com.nokia.QtDev-i686-w64-Npackd-Release-5.5
+set PACKAGE=com.googlecode.windows-package-manager.Npackd
+set mingw_libs=i686-w64-mingw32
+
+set onecmd="%npackd_cl%\ncl.exe" path -p mingw-w64-i686-sjlj-posix -v 4.9.2
+for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set mingw=%%x
+
+set onecmd="%npackd_cl%\ncl.exe" path -p quazip-dev-i686-w64-static -v 0.7.1
+for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set quazip=%%x
+
+set onecmd="%npackd_cl%\ncl.exe" path -p z-dev-i686-w64_sjlj_posix_4.9.2-static -v 1.2.11
+for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set zlib=%%x
+
+set onecmd="%npackd_cl%\ncl.exe" path -p drmingw -v 0.7.7
+for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set drmingw=%%x
+
+goto start
+
+:bits64
+
+set QT=C:\NpackdSymlinks\com.nokia.QtDev-x86_64-w64-Npackd-Release-5.5
+set PACKAGE=com.googlecode.windows-package-manager.Npackd64
+set mingw_libs=x86_64-w64-mingw32
+
+set onecmd="%npackd_cl%\ncl.exe" path -p mingw-w64-x86_64-seh-posix -v 4.9.2
+for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set mingw=%%x
+
+set onecmd="%npackd_cl%\ncl.exe" path -p quazip-dev-x86_64-w64-static -v 0.7.1
+for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set quazip=%%x
+
+set onecmd="%npackd_cl%\ncl.exe" path -p z-dev-x86_64-w64_seh_posix_4.9.2-static -v 1.2.11
+for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set zlib=%%x
+
+set onecmd="%npackd_cl%\ncl.exe" path -p drmingw64 -v 0.7.7
+for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set drmingw=%%x
+
+goto start
+
+:start
 cd npackdcl
 cd tests
 
