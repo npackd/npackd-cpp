fbd3a610eccb2f1e5cef0b004a10f964eb330363
tim-lebedkov <tim.lebedkov@gmail.com>
2020-07-06 10:44:02 +0200

    DISM

---
+++ npackdg/src/dismthirdpartypm.cpp
@@ -0,0 +1,6 @@
+#include "dismthirdpartypm.h"
+
+DISMThirdPartyPM::DISMThirdPartyPM()
+{
+
+}
---
+++ npackdg/src/dismthirdpartypm.h
@@ -0,0 +1,11 @@
+#ifndef DISMTHIRDPARTYPM_H
+#define DISMTHIRDPARTYPM_H
+
+
+class DISMThirdPartyPM
+{
+public:
+    DISMThirdPartyPM();
+};
+
+#endif // DISMTHIRDPARTYPM_H
