634e9f5837112a8d2da51bcafeae2053a2d92808
tim-lebedkov <tim.lebedkov@gmail.com>
2021-02-27 12:16:48 -0800

    more useful GCC warning settings

--- cmake/Common.cmake
+++ cmake/Common.cmake
@@ -10,12 +10,7 @@ endif()
 
 # compiler warnings
 if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANGXX OR CMAKE_COMPILER_IS_GNUCC)
-  set(NPACKD_WARNING_FLAGS "-Wall -Winit-self -Wwrite-strings -Wextra -Wno-long-long -Wno-overloaded-virtual -Wno-missing-field-initializers -Wno-unused-parameter -Wno-unknown-pragmas -Wno-cast-function-type")
-  if(EMSCRIPTEN)
-    set(NPACKD_WARNING_FLAGS "${NPACKD_WARNING_FLAGS} -Wno-warn-absolute-paths")
-  elseif(NOT APPLE)
-    set(NPACKD_WARNING_FLAGS "${NPACKD_WARNING_FLAGS} -Wno-unused-but-set-parameter")
-  endif()
+  set(NPACKD_WARNING_FLAGS "-Wall -Wwrite-strings -Wextra -Wno-unused-parameter -Wno-cast-function-type -Wduplicated-cond -Wduplicated-branches -Wlogical-op")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${NPACKD_WARNING_FLAGS}")
 endif()
 
