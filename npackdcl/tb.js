27deece87d27a4a1c06b1d0e283d845e5e6710c8
tim/lebedkov <tim.lebedkov@gmail.com>
2023-05-01 17:01:09 +0200

    NpackdCL

---
+++ npackdcl/tb.js
@@ -0,0 +1,16 @@
+// @ts-check
+/// <reference path="../../super/tb/install/builtins.js"/>
+
+// TODO: src/app.rc.in
+// TODO: src/app.ico
+// TODO: npackdcl.rc
+// TODO: qsqlite
+// TODO: qtpcre2 zstd z
+// TODO: ../CrystalIcons_LICENSE.txt ../LICENSE.txt 
+// TODO: npackdcl.manifest
+project.setName("npackdcl");
+project.setType("program");
+project.setDependencies(["npackd", "quazip1-qt5", "Qt5Sql", "Qt5Xml", "Qt5Core", "oleaut32", "userenv", "winmm", "ole32",
+    "uuid", "wininet", "psapi", "version", "shlwapi", "msi", "netapi32", "Ws2_32", "taskschd"]);
+project.setQtMocFiles(["app.h"]);
+system.include("..\\tb_common.js");
