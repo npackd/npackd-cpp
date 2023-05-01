f684ec36eeb13cb891db7fa7d6f883a6785dc816
tim/lebedkov <tim.lebedkov@gmail.com>
2023-05-01 16:53:11 +0200

    - Qt MOC
    - CLU

--- clu/tb.js
+++ clu/tb.js
@@ -8,6 +8,7 @@
 // TODO: ../LICENSE.txt
 project.setName("clu");
 project.setType("program");
-project.setDependencies(["npackd", "quazip1-qt5", "Qt5Sql", "Qt5Xml", "Qt5Core", "userenv", "winmm", "ole32",
+project.setDependencies(["npackd", "quazip1-qt5", "Qt5Sql", "Qt5Xml", "Qt5Core", "oleaut32", "userenv", "winmm", "ole32",
     "uuid", "wininet", "psapi", "version", "shlwapi", "msi", "netapi32", "Ws2_32", "taskschd"]);
 system.include("..\\tb_common.js");
+
--- npackd/tb.js
+++ npackd/tb.js
@@ -6,3 +6,5 @@ project.setType("library");
 project.setDependencies(["quazip1-qt5", "Qt5Sql", "Qt5Xml", "Qt5Core", "userenv", "winmm", "ole32",
     "uuid", "wininet", "psapi", "version", "shlwapi", "msi", "netapi32", "Ws2_32", "taskschd"]);
 system.include("..\\tb_common.js");
+project.setQtMocFiles(["clprogress.h", "downloader.h", "installedpackages.h", "job.h"]);
+
