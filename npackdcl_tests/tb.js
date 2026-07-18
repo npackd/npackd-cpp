30cf523b49eea608baf6dde9593c61bd26419f78
tim-lebedkov <tim.lebedkov@gmail.com>
2026-07-18 18:27:10 +0200

    tb.js for npackd-cpp

--- npackdcl_tests/tb.js
+++ npackdcl_tests/tb.js
@@ -6,6 +6,8 @@ function configure() {
     project.setVariable("TYPE", "program");
     project.setVariable("DEPENDENCIES", ["npackd", "Qt5Test" ]);
     
+    var static_ = project.getConfig().indexOf("static") >= 0;
+
     project.setVariable("QT_MOC_FILES", ["app.h"]);
     system.include("..\\tb_common.js");
 
@@ -15,6 +17,11 @@ function configure() {
     project.findPkgConfigLibrary("Qt5Core", { static: static_ });
     project.findPkgConfigLibrary("Qt5Test", { static: static_ });
 
-    if (!static_)
+    if (!static_) {
         project.findPkgConfigLibrary("quazip1-qt5");
+    } else {
+        var qt = "C:\\msys64\\mingw64\\qt5-static\\";
+        var qt_plugins = qt + "share\\qt5\\plugins\\";
+        project.setLibraryPath("qsqlite", qt_plugins + "sqldrivers\\libqsqlite.a");
+    }
 }
---
+++ tb.js
@@ -0,0 +1,50 @@
+// @ts-check
+/// <reference path="../super/tb/install/builtins.js"/>
+
+function configure() {
+    var static_ = project.getConfig().indexOf("static") >= 0;
+
+    project.setVariable("NAME", "npackd-cpp");
+    project.setVariable("TITLE", "Npackd C++");
+    project.setVariable("DESCRIPTION", "All the C++ Npackd programs.");
+    project.setVariable("TYPE", "generic");
+}
+
+function build() {
+    console.log("Building Npackd C++................");
+
+    var all = ["clu", "npackd", "npackdcl", "npackdcl_ftests", "npackdcl_tests", "npackdg"];
+
+    for (var i = 0; i < all.length; i++) {
+        var name = all[i];
+        console.log("We are building " + name + "................");
+        var p = new Project(project.getDirectory() + "\\" + name);
+        p.setConfig(project.getConfig());
+        p.build("all");
+    }
+
+    // TODO: exeproxy.exe, exchndl.dll, mgwhelp.dll, dbghelp.dll, symsrv.dll, symsrv.yes, xxx.map
+    // TODO: create a zip file
+    // TODO: create .msi
+    // TODO: release on Github
+
+    // TODO: Coverity
+    /*
+rem Coverity build is too slow
+
+set path=%mingw%\bin;%ai%\bin\x86;%sevenzip%;C:\msys64\mingw64\bin
+set CMAKE_PREFIX_PATH=%mingw%\%mingw_libs%
+mingw32-make.exe clean
+
+"..\..\cov-analysis\bin\cov-configure.exe"  --comptype gcc --compiler C:\PROGRA~2\MINGW-~1\bin\G__~1.EXE -- -std=gnu++11
+"..\..\cov-analysis\bin\cov-build.exe" --dir ..\..\cov-int mingw32-make.exe install
+
+rem type C:\projects\Npackd\cov-int\build-log.txt
+
+7z a cov-int.zip cov-int
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+"C:\Program Files (x86)\Gow\bin\curl" --form token=%covtoken% --form email=tim.lebedkov@gmail.com -k --form file=@cov-int.zip --form version="Version" --form description="Description" https://scan.coverity.com/builds?project=Npackd
+if %errorlevel% neq 0 exit /b %errorlevel%
+    */
+}
